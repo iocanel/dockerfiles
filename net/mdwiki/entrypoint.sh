104f3122a6f5b09939963954a0b880249d9f9236
Ioannis Canellos <iocanel@gmail.com>
2021-12-28 16:55:48 +0200

    chore: improvements on mdwiki image.

--- net/mdwiki/entrypoint.sh
+++ net/mdwiki/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-if [ ! -f /wiki/index.md ]; then
-    mkdir -p /tmp/wiki
-    echo "Wiki not found!"
-    echo "# mdwiki" > /tmp/wiki/index.md
-    echo "---" >> /tmp/wiki/index.md
-    echo "Your wiki is empty please add content to the wiki directory (defaults to $HOME/wiki) and rerun!" >> /tmp/wiki/index.md
-    http-server /tmp/wiki
-else
-    http-server /wiki
+mkdir -p ~/wiki
+if [ ! -f ~/wiki/index.html ]; then
+    cp /usr/local/mdwiki/defaults/index.html ~/wiki
 fi
+
+if [ ! -f ~/wiki/index.md ]; then
+    cp /usr/local/mdwiki/defaults/index.md ~/wiki
+fi
+
+http-server ~/wiki
